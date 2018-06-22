c300a23cea24e6c414e35e6b7764782590bcddbd
Anderson Luiz Vendruscolo <andersonlven@gmail.com>
2018-06-22 17:13:57 -0300

    removido hello padrao do projeto

--- lib/bc_investiments.ex
+++ lib/bc_investiments.ex
@@ -1,18 +1,3 @@
 defmodule BcInvestiments do
-  @moduledoc """
-  Documentation for BcInvestiments.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> BcInvestiments.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  
 end
